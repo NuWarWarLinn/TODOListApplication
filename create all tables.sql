7ccef3420e9d661426cf27bc294f0d7abc2e7cb4
NuWarWarLinn <nuwar.oct24@gmail.com>
2020-04-25 19:13:43 +0630

    Update create all tables.sql

--- create all tables.sql
+++ create all tables.sql
@@ -1,45 +1,39 @@
-DROP TABLE IF EXISTS user;
-create table user
-(user_id int NOT NULL,
-user_name varchar(200) NOT NULL);
+CREATE TABLE DEPARTMENT (
+    DEPARTMENT_ID       INT         NOT NULL,
+    DEPARTMENT_NAME   VARCHAR(255)         NOT NULL,
+    CONSTRAINT DEP_PK1 PRIMARY KEY(DEPARTMENT_ID));
 
-DROP TABLE IF EXISTS japaneselevel;
-create table japaneselevel
-(japaneselevel_id int NOT NULL,
-japaneselevel_name varchar(500) NOT NULL,
-CONSTRAINT contacts_pk PRIMARY KEY (japaneselevel_id));
+CREATE TABLE POSITION (
+    POSITION_ID       INT         NOT NULL,
+    POSITION_NAME   VARCHAR(255)         NOT NULL,
+    CONSTRAINT POS_PK1 PRIMARY KEY(POSITION_ID));
 
+CREATE TABLE JAPANESE_LEVEL(
+    LEVEL_ID       INT         NOT NULL,
+    LEVEL_NAME   VARCHAR(255)         NOT NULL,
+    CONSTRAINT JAP_PK1 PRIMARY KEY(LEVEL_ID));
 
-DROP TABLE IF EXISTS department;
-create table department
-(department_id int NOT NULL,
-department_name varchar(500) NOT NULL,
-CONSTRAINT contacts_pk PRIMARY KEY (department_id));
+CREATE TABLE AUTHORITY(
+    AUTHORITY_ID       INT         NOT NULL,
+    AUTHORITY_NAME   VARCHAR(255)         NOT NULL,
+    CONSTRAINT AUT_PK1 PRIMARY KEY(AUTHORITY_ID));
 
-DROP TABLE IF EXISTS position;
-create table position
-(position_id int NOT NULL,
-position_name varchar(500) NOT NULL,
-CONSTRAINT contacts_pk PRIMARY KEY (position_id));
-
-DROP TABLE IF EXISTS employee;
-create table employee
-(employee_id int NOT NULL,
-employee_name varchar(500) NOT NULL,
-joinDate date NOT NULL,
-position varchar(200) NOT NULL,
-email varchar(400) NOT NULL,
-phone_no varchar(200) NOT NULL,
-birth_date date NOT NULL,
-marital_status varchar(100) NOT NULL,
-sex varchar(10) NOT NULL,
-japaneselevel_id int NOT NULL,
-department_id int NOT NULL,
-position_id int NOT NULL,
-CONSTRAINT contacts_pk PRIMARY KEY (employee_id),
-FOREIGN KEY (japaneselevel_id) REFERENCES japaneselevel(japaneselevel_id),
-FOREIGN KEY (department_id) REFERENCES department(department_id),
-FOREIGN KEY (position_id) REFERENCES position(position_id));
+CREATE TABLE EMPLOYEE (
+    EMPLOYEE_ID      CHAR(10)         NOT NULL,
+    EMPLOYEE_NAME    VARCHAR(255)     NOT NULL,
+    BIRTHDATE        DATE             NOT NULL,
+    GENDER           CHAR(1)          NOT NULL,
+    MARITAL_STATUS   CHAR(1)          NOT NULL,
+    LEVEL_ID         INT              NOT NULL,
+    DEPARTMENT_ID    INT              NOT NULL,
+    JOIN_DATE        DATE             NOT NULL,
+    POSITION_ID      INT              NOT NULL,
+    EMAIL            VARCHAR(255)     NOT NULL,
+    PHONE            VARCHAR(15)      NOT NULL,
+    CONSTRAINT EMP_PK1 PRIMARY KEY(EMPLOYEE_ID),
+       FOREIGN KEY (DEPARTMENT_ID) REFERENCES DEPARTMENT(DEPARTMENT_ID),
+       FOREIGN KEY (LEVEL_ID) REFERENCES JAPANESE_LEVEL(LEVEL_ID),
+       FOREIGN KEY (POSITION_ID) REFERENCES POSITION(POSITION_ID));
 
 COMMIT;
 
